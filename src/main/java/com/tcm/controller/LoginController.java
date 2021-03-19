1336510505694717049862ae7f48b71ff2dafba6
Kobayashi <you@example.com>
2021-03-20 00:01:51 +0900

    不要なもの削除、インデント修正

--- src/main/java/com/tcm/controller/LoginController.java
+++ src/main/java/com/tcm/controller/LoginController.java
@@ -1,10 +1,9 @@
-package com.tcm.controller.sample;
+package com.tcm.controller;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -14,14 +13,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.tcm.dto.login.UserModel;
 import com.tcm.enums.Role;
-import com.tcm.repository.SampleMapper;
-import com.tcm.repository.SampleXmlVersionMapper;
 
 @Controller
 public class LoginController {
-
-	@Autowired SampleMapper mapper;
-	@Autowired SampleXmlVersionMapper xmlVersionMapper;
 
     @RequestMapping ("/login_success")
     public String success(@AuthenticationPrincipal UserModel userModel, ModelAndView mv) {
